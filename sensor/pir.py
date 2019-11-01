bea112984059321a0ae420c9b69b2afad98c0c04
mayankmetha <mayankmethad@gmail.com>
2019-11-01 12:08:00 +0530

    added requests to camera

--- sensor/pir.py
+++ sensor/pir.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
+import requests
+import sys
+if len(sys.argv) != 2:
+    print("Error: python3 <script> <ip address>")
+    exit(0)
+ip = sys.argv[1]
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN)
@@ -9,10 +15,11 @@ try:
     while True:
         i = GPIO.input(8)
         if i != old_i:
+            r = requests.get(ip+":5000/peopleCount")
             f = open('../raw/pir0','w')
-            f.write(str(i))
+            f.write(str(r.text))
             f.close()
             old_i = i
-        time.sleep(0.1)
+        time.sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
