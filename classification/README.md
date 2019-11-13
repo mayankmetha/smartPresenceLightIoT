45b582e59a007cfb06681c9e1b1858b5c1569008
Prashanth Ravoor <prash.ravoor125@gmail.com>
2019-11-13 12:01:53 +0530

    Changed People detection model from YOLO to MobileNet-SSD. Model now uses tensorflow

--- classification/README.md
+++ classification/README.md
@@ -1,23 +1,27 @@
 # Counting the number of people in the room
-To count the number of people in the room, a camera will be used. The camera image captured from the video stream will be run through a [YOLO](https://pjreddie.com/darknet/yolo/) Deep Neural Network to find if there are people present in the room, and then find the count of people. <br>
+To count the number of people in the room, a camera will be used. The camera image captured from the video stream will be run through a [MobileNetV2-SSD](https://pjreddie.com/darknet/yolo/) Deep Neural Network to find if there are people present in the room, and then find the count of people. <br> 
 
 ## REST Server
-The application access is through REST API, called via `GET http://<IP:5000>/classify` <br>
-The response is in the format:
-```json
-{
-    'numPersons': int
-}
-```
+The application access is through REST API, called via `GET http://<IP:5000>/countPeople` <br>
+The response is simply an integer representing the number of people found. <br>
 
 ## Setup
 Python 3.6+ is required. Install all necessary dependencies through: <br>
 `python -m pip install -r requirements.txt` <br>
 
 This will take care of installing any necessary modules <br>
-For YOLO, the pre-trained model weights are needed for the network to classify the image. They can be downloaded from the YOLO webiste [here](https://pjreddie.com/media/files/yolov3.weights) (237MB). <br>
+For detection to work, the pre-trained model weights are needed for the network to classify the image. They can be downloaded from the Tensorflow webiste [here](http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_oid_v4_2018_12_12.tar.gz) (152MB). <br>
 <br>
-Save the weights file directly under the `classification` sub-folder under the root folder. <br>
+Extract the contents of the tarball, and retain the `saved_model` folder. Ensure that the `saved_model` folder is under a directory named `ssd`. The folder structure looks like: <br>
+```
++ root
+  + classification
+    - README.md
+    + ssd
+        + saved_model
+        - label_mapping.csv
+  ...
+```
 
 ### Camera
 Currently, the phone camera is used in place of an actual camera <br>
@@ -30,7 +34,7 @@ CAMERA_IP = '<camera IP address:Port>' (e.g. '192.168.1.2:8080')
 
 ## Running the application
 Start the REST server using `FLASK_APP=rest_server_classifier flask run` <br>
-Make a HTTP GET call to `<server IP>:5000/peopleCount` (using a browser will also work) to run get the number of people in the room. <br>
+Make a HTTP GET call to `<server IP>:5000/countPeople` (using a browser will also work) to run get the number of people in the room. <br>
 
 The file can also be run directly as a python script through `python rest_server_classifier.py`, which simply grabs a single frame from the camera, and returns the number of people found. <br>
 
--- classification/rest_server_classifier.py
+++ classification/rest_server_classifier.py
@@ -1,13 +1,8 @@
 import cv2
-import cv2.dnn as dnn
 import numpy as np
 import matplotlib.pyplot as plt
 from flask import Flask
-
-def get_output_layers(net):    
-    layer_names = net.getLayerNames()    
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-    return output_layers
+from object_detection import ObjectDetector
 
 # function to draw bounding box on the detected object with class name
 def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h, classes=[]):
@@ -17,16 +12,6 @@ def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h, class
     
     cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), 1, 2)
     cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, 4)
-
-def loadModel(conf, weights):
-    return dnn.readNet(conf, weights)
-
-def loadClassNames(filename):
-    classes = []
-    with open(filename) as f:
-        classes = list(map(lambda x: x.strip(), f.readlines()))
-        f.close()
-    return classes
 
 def readImage(filename='', ip=''):
     if len(filename) > 0:
@@ -41,83 +26,48 @@ def readImage(filename='', ip=''):
             image = None
     return image
 
-def classifyImage(net, image):
-    blob = dnn.blobFromImage(image, 1/255, (416,416), (0,0,0), True, crop=False)
-    net.setInput(blob)
-    return net.forward(get_output_layers(net))
+def loadClasses(file):
+    classNames = {}
+    with open(file) as f:
+        classNames = { int(line.split(',')[0]) : line.split(',')[1].strip() for line in f.readlines() }
+        f.close()
+    return classNames
 
-def getBoundingBoxes(outs, width, height, conf_threshold = 0.8, nms_threshold=0.45):
-    class_ids = []
-    confidences = []
-    boxes = []
-
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > conf_threshold:
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                x = (center_x - w/2.0)
-                y = (center_y - h/2.0)
-                class_ids.append(class_id)
-                confidences.append(float(confidence))
-                boxes.append([x, y, w, h])
-    return class_ids, confidences, boxes
-
-def drawBoundingBoxesToImage(test, boxes, confidences, classes, class_ids, conf_threshold, nms_threshold):
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-    bboxes = []
-
-    for i in indices:
-        i = i[0]
-        box = boxes[i]
-        x = round(box[0])
-        y = round(box[1])
-        w = box[2]
-        h = box[3]
-
-        print(classes[class_ids[i]])
-        draw_bounding_box(test, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h), classes)
-        bboxes.append(image[y:y+h, x:w+x])
-    plt.imshow(test)
-
-def countPeople(net, image, classes, person_class):
-    width = image.shape[1]
-    height = image.shape[0]
-    conf_threshold = 0.1
-    nms_threshold=0.45
-    outs = classifyImage(net, image)
-    class_ids,confidences,boxes = getBoundingBoxes(outs, width, height, conf_threshold=conf_threshold, nms_threshold=nms_threshold)
-    drawBoundingBoxesToImage(image, boxes, confidences, classes, class_ids, conf_threshold=conf_threshold, nms_threshold=nms_threshold)
-    
-    return len(list(filter(lambda x: x == person_class, class_ids)))
+def countPeople(image):
+    result = det.getBoundingBoxes(image)
+    print(result)
+    numPeople = 0
+    for res in result:
+        name = classes[res.classid].lower()
+        if name == 'person' or name == 'man' or name == 'woman' or  \
+                name == 'boy' or name == 'girl' or 'human' in name:
+            numPeople += 1
+    return numPeople
 
 
-net = loadModel('cfg/yolov3.cfg', 'yolov3.weights')
-classes = loadClassNames('data/coco.names')
-CAMERA_IP='192.168.1.2:8080'
-# net = loadModel('cfg/darknet19.cfg', 'darknet19.weights')
-# classes = loadClassNames('imagenet.shortnames.list')
+CAMERA_IP = '<CAMERA IP>:<PORT>'
+print('Loading Model...')
+det = ObjectDetector('ssd/saved_model')
+det.loadModel()
+print('Model Load completed...')
+print('Warming up Model...')
+det.getBoundingBoxes(readImage(ip=CAMERA_IP))
+det.getBoundingBoxes(readImage(ip=CAMERA_IP))
+
+classes = loadClasses('ssd/label_mapping.csv')
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
     image = readImage(ip=CAMERA_IP)
-
-    numPersons = countPeople(net, image, classes, 0)
+    numPersons = countPeople(image)
     print('Found {} people in the image!'.format(numPersons))
-    
 
 @app.route('/countPeople')
 def getPersonCount():
     image = readImage(ip=CAMERA_IP)
-
-    numPersons = countPeople(net, image, classes, 0)
+    numPersons = countPeople(image)
+    del image
     print('Found {} people in the image!'.format(numPersons))
-    # plt.show()
-    return { 'numPersons': numPersons }
+    return str(numPersons)
 
